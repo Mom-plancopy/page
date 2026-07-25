5138f222fe18a12dc5dd6b4c8f13d711a8335741
Mom-plancopy <kanthakorn.k@gmail.com>
2026-07-25 07:51:39 +0700

    Update file.lua

--- file.lua
+++ file.lua
@@ -1,3 +1,15 @@
+--------------------------------------------------------------------------------------------------------------------
+plr = game:GetService("Players").LocalPlayer
+plrgui = game.CoreGui
+plrscr = plr.PlayerScripts
+sounds = game:GetService("SoundService")
+startergui = game.StarterGui
+repl = game:GetService("ReplicatedStorage")
+replc = game:GetService("ReplicatedFirst")
+mouse = plr:GetMouse()
+chat = game:GetService("TextChatService")
+--------------------------------------------------------------------------------------------------------------------
+
 local Rayfield = loadstring(game:HttpGet("https://sirius.menu/gen2"))()
 
 local window = Rayfield:CreateWindow({
@@ -21,5 +33,27 @@ tab:CreateButton({
                 {text = "Unload",style = "danger",callback = function() window:Unload() end},
             },
         })
+    end,
+})
+
+tab:CreateSlider({
+    name = "Field of view",
+    range = { 70, 120 },
+    increment = 1,
+    value = workspace.CurrentCamera.FieldOfView,
+    suffix = "°",
+    callback = function(value)
+        workspace.CurrentCamera.FieldOfView = value
+    end,
+})
+
+tab:CreateSlider({
+    name = "Walkspeed",
+    range = { 8, 200 },
+    increment = 1,
+    value = plr.Character.Humanoid.WalkSpeed,
+    suffix = "stud/min",
+    callback = function(value)
+        plr.Character.Humanoid.WalkSpeed = value
     end,
 })
